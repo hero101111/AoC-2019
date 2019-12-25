6f2004a7a904bf5071c927840ba2d171fd47bebf
Gabriel Diaconita <diaconitagabriel@gmail.com>
2019-12-25 08:28:34 +0200

    Add solution for day 25

--- AocSolutions/days/25/Day25.h
+++ AocSolutions/days/25/Day25.h
@@ -1,35 +1,43 @@
 #pragma once
 
 #include "SolutionDay.h"
-#include "util/util.h"
-#include "util/IntComputer.h"
 
 class Day25 : public ISolutionDay
 {
 private:
 
-  vector<string>  mdata;
-
 public:
-
-  Day25() { }
 
   ~Day25() override { }
 
-  void ReadData()
-  {
-    mdata = rff(KINPUT "25\\input.txt");
-  }
-
   string Part1() override
   {
-    int ret = 251;
-    return std::to_string(ret);
+    IntComputer cpu{ rff(KINPUT "25\\input.txt")[0] };
+
+    cpu.mInputFunction = [&]() 
+    {
+      char c;
+      cin >> noskipws >> c;
+      return c;
+    };
+
+    cpu.mOutputCallback = [&](int output) 
+    {
+      cout << (char)output;
+    };
+
+    cpu.Execute();
+
+    // after a lot of mazing around the ship
+    // for this input you'll need :
+    // MUG, HYPERCUBE, COIN, ASTROLABE
+    
+    // this unlocks the answer below
+    return std::to_string(1077936448);
   }
 
   string Part2() override
   {
-    int ret = 252;
-    return std::to_string(ret);
+    return "50 STARS, HO HO HO. MERRY CHRISTMAS!";
   }
 };
